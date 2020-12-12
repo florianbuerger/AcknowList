20bf79ee5b855d156b7571f5d3b09a2bb049db3f
Florian Buerger <florian@florianbuerger.com>
2020-12-12 12:08:52 +0100

    Fix integration warning on Xcode 12+

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "AcknowList",
     platforms: [
-        .iOS(.v8), .tvOS(.v9)
+        .iOS(.v9), .tvOS(.v9)
     ],
     products: [
         .library(name: "AcknowList", targets: ["AcknowList"])
